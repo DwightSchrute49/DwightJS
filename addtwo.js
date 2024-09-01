49e957d06fee7a7727ddbf396f174734d282baf7
DwightSchrute49 <159871146+DwightSchrute49@users.noreply.github.com>
2024-09-01 19:41:32 +0530

    JS code

---
+++ addtwo.js
@@ -0,0 +1,11 @@
+// Function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Example usage
+const number1 = 5;
+const number2 = 7;
+const sum = addNumbers(number1, number2);
+
+console.log("The sum of " + number1 + " and " + number2 + " is " + sum + ".");
